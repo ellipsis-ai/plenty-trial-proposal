bb1eae81e821fc954dd8e2404044a62a55fc7711
Andrew Catton <>
2019-07-26 18:08:39 +0000

    put the new proposal sheet copy into a specific dir; also cleaned up env var names

--- actions/new/function.js
+++ actions/new/function.js
@@ -3,12 +3,12 @@ function(name, ellipsis) {
 const {Sheet} = ellipsis.require('ellipsis-gsheets@^0.0.1');
 const SlackUser = require('SlackUser');
 
-drive.copyFile(ellipsis.env.TRIAL_POLICY_TEMPLATE_SHEET_ID).
+drive.copyFile(ellipsis.env.TRIAL_PROPOSAL_TEMPLATE_SHEET_ID).
   then(res => {
     return drive.makeWritable(res.data.id).then(() => res.data.id);
   }).
   then(fileId => {
-    const trackingSheet = new Sheet(ellipsis, ellipsis.env.TRIAL_POLICY_PROGRESS_SHEET_ID);
+    const trackingSheet = new Sheet(ellipsis, ellipsis.env.TRIAL_PROPOSAL_PROGRESS_SHEET_ID);
     const sheetUrl = `https://docs.google.com/spreadsheets/d/${fileId}`;
     const submitter = new SlackUser(ellipsis.event.user.userIdForContext, ellipsis.event.user.userName);
     const row = [name, sheetUrl, submitter.cellText()];
